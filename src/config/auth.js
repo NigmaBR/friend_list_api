08dd280df2c010ce23d1e668c857683d407b1b5c
Ricardo Junior <ricardojunior199202@gmail.com>
2019-12-19 20:26:13 -0300

    Models e Controllers

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1451e52cfb57602f8dd70a0244b609b0',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'lista',
+  database: 'baselista',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
